2b198d63d9508bbdced1ee389b2ab3ec7a86bbe4
donald.ahossi <donaldalphonso11@gmail.com>
2025-05-16 22:51:17 +0200

    Optimizing Complex Queries

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,60 @@
+-- Requête initiale avec plusieurs jointures
+SELECT 
+    B.booking_id,
+    B.start_date,
+    B.end_date,
+    B.total_price,
+    
+    U.user_id,
+    U.first_name,
+    U.last_name,
+    U.email,
+    
+    P.property_id,
+    P.name AS property_name,
+    P.location,
+    P.pricepernight,
+    
+    PAY.payment_id,
+    PAY.amount,
+    PAY.payment_method,
+    PAY.payment_date
+
+FROM Booking B
+INNER JOIN User U ON B.user_id = U.user_id
+INNER JOIN Property P ON B.property_id = P.property_id
+LEFT JOIN Payment PAY ON PAY.booking_id = B.booking_id;
+
+-- Initial full query (no filter)
+EXPLAIN SELECT 
+    B.booking_id,
+    B.start_date,
+    B.end_date,
+    B.total_price,
+    
+    U.user_id,
+    U.first_name,
+    U.last_name,
+    U.email,
+    
+    P.property_id,
+    P.name AS property_name,
+    P.location,
+    P.pricepernight,
+    
+    PAY.payment_id,
+    PAY.amount,
+    PAY.payment_method,
+    PAY.payment_date
+
+FROM Booking B
+JOIN User U ON B.user_id = U.user_id
+JOIN Property P ON B.property_id = P.property_id
+LEFT JOIN Payment PAY ON PAY.booking_id = B.booking_id;
+
+-- Query with filter to test index
+EXPLAIN ANALYZE
+SELECT ...
+FROM Booking B
+JOIN User U ON ...
+WHERE B.user_id = 'some-existing-id';
