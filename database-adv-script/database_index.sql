ea66996aa3e1b23a6dbfadf9d55c73d4f8e17f21
donald.ahossi <donaldalphonso11@gmail.com>
2025-05-16 22:30:07 +0200

    adding EXPLAIN ANALYZE

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -16,3 +16,9 @@ CREATE INDEX idx_review_property ON Review(property_id);
 -- Explain
 EXPLAIN SELECT * FROM Booking WHERE user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12';
 
+-- Check query plan after index
+EXPLAIN ANALYZE
+SELECT *
+FROM Booking
+WHERE user_id = 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12';
+
